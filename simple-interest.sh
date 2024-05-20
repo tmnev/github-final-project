ee2be9b66694c15e17811acb2ba498c582846bfd
tmnev <tmnevala@gmail.com>
2024-05-20 16:01:44 +0300

    added new file browser support

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # tmnev
+   # tmnev (Tom)
    # Input:
    # p, principal amount
    # t, time period in years
